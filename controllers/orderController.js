0d37880cd5bc9fd14b657a8550de58c2a106aa49
MohamedTarek <mt469063@gmail.com>
2023-10-13 15:56:07 +0300

    create order with card

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -159,7 +159,7 @@ const chekoutSession=asyncHandler(async(req,res,next)=>{
 const createCartOrder=async(session)=>{
     const cartId = session.client_reference_id;
     const shippingAddress = session.metadata;
-    const orderPrice=session.display_items[0].amount /100;
+    const orderPrice=session.amount_total /100;
 
     const cart =await Cart.findById(cartId);
     const user=await User.findOne({email:session.customer_email});
