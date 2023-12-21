6b0e665553d11bba49bc77db9013d0776b3074fe
MohamedTarek <mt469063@gmail.com>
2023-12-21 23:41:37 +0200

    inhancements

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://e-commerce-front-woad-phi.vercel.app');
     // Add other headers if needed
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
 
