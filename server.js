52ff2c24f76496178c85e14d4400f18746fb84ea
MohamedTarek <mt469063@gmail.com>
2023-12-21 23:52:10 +0200

    inhancements

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app=express();
 //enable other domains to access your application
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://e-commerce-front-woad-phi.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Add other headers if needed
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
